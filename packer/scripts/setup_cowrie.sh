57a442f4f2f10d0ad5bd1c8da3d7f915719b2665
Adam <adam.winargio@gmail.com>
2022-11-17 17:27:18 +0100

    add cowrie user restriction
    restrict which username-password combination to access the honeypot

--- packer/scripts/setup_cowrie.sh
+++ packer/scripts/setup_cowrie.sh
@@ -10,5 +10,8 @@ pip install --upgrade pip
 pip install --upgrade -r requirements.txt
 
 mv /home/cowrie/cowrie.cfg /home/cowrie/cowrie/etc/
+mv /home/cowrie/userdb.txt /home/cowrie/cowrie/etc/
 mv /home/cowrie/cowrie.sh /
+mv /home/cowrie/activate_honeypot_module.sh /
+
 chown -R cowrie:cowrie /home/cowrie/cowrie
