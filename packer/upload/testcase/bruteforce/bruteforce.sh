bb350f6e2a68072353db40b946beef62423cca74
Adam <adam.winargio@gmail.com>
2023-02-26 11:09:54 +0100

    small fix for brute force testcase

--- packer/upload/testcase/bruteforce/bruteforce.sh
+++ packer/upload/testcase/bruteforce/bruteforce.sh
@@ -5,4 +5,4 @@ cd /testcase/
 source /testcase/attacker-env/bin/activate
 pip install paramiko colorama
 cd bruteforce
-python3 bruteforce-ext.py 172.17.0.2 -u usernames.txt -P passwords.txt
+python3 bruteforce.py 172.17.0.2 -u usernames.txt -P passwords.txt
