fadfb5267a24d800f9c9554bcef2a106e570416c
Adam <adam.winargio@gmail.com>
2023-03-04 17:07:46 +0100

    update activating script, add malware-detector on activation

--- packer/upload/honeypot/activate_honeypot_module.sh
+++ packer/upload/honeypot/activate_honeypot_module.sh
@@ -3,4 +3,6 @@ python3 /home/cowrie/modules/portscan_detector/portscans.py -d -f "/home/cowrie/
 source /home/cowrie/cowrie/cowrie-env/bin/activate
 mkdir -p /home/cowrie/modules/dos_detector/data 
 python3 /home/cowrie/modules/dos_detector/dos-detector.py &
+python3 /home/cowrie/modules/malware_detector/malware.py &
+touch /home/cowrie/cowrie/var/log/cowrie/honeypot.log
 tail -f /home/cowrie/cowrie/var/log/cowrie/cowrie.log
