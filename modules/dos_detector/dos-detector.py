70813dd977379d09428448fd785e563cde4e7f36
Adam <adam.winargio@gmail.com>
2023-03-05 11:49:08 +0100

    fix dos detector: detect only flood, fix xactivate: now tail honeypot's log

--- modules/dos_detector/dos-detector.py
+++ modules/dos_detector/dos-detector.py
@@ -13,18 +13,19 @@ def handler(packet):
     host_name=socket.gethostname()   
     host_ip=socket.gethostbyname(host_name)
     packet_summary = packet.summary() + " " + dt
+
     if packet.haslayer(ICMP) or "icmp" in packet_summary:
         if packet.getlayer(IP).src != host_ip and "echo-reply" in packet_summary:
             msg = log_time + " [DOS]: Smurf from {} (spoofed).".format(packet.getlayer(IP).src)
             append_to_log(msg)
             append_to_file(packet_summary, "/home/cowrie/modules/dos_detector/data/data_smurf")
         else:
-            if(is_icmp_flood(packet_summary)):
+            if(is_icmp_flood(packet_summary, datetime_today, host_ip)):
                 msg = log_time + " [DOS]: ICMP-Flood from {}.".format(packet.getlayer(IP).src)
-            append_to_log(msg)
+                append_to_log(msg)
             append_to_file(packet_summary, "/home/cowrie/modules/dos_detector/data/data_ping_icmp")
     elif packet.haslayer(TCP) or "tcp" in packet_summary:
-        if packet.haslayer(Raw) and not host_ip+":2222" in packet_summary:
+        if packet.haslayer(Raw) and not host_ip+":2222" in packet_summary and not 'PA / Raw' in packet_summary:
             if(is_tcp_flood(packet_summary)):
                 msg = log_time + " [DOS]: TCP-Flood from {}.".format(packet.getlayer(IP).src)
                 append_to_log(msg)
@@ -39,16 +40,20 @@ def append_to_file(msg, file_name):
 def append_to_log(msg):
     if not is_alerted(msg):
         file_log = '/home/cowrie/cowrie/var/log/cowrie/honeypot.log'
-        file_alert = '/home/cowrie/modules/dos_detector/data/data_alerted'
         append_to_file(msg, file_log)
-        append_to_file(msg, file_alert)
+
+def count_delta(datetime_now, readed_datetime):
+    if isinstance(datetime_now, str):
+        dt_a = datetime.strptime(datetime_now, '%Y-%m-%d %H:%M:%S')
     else:
-        pass
+        dt_a = datetime_now
+    dt_b = datetime.strptime(readed_datetime, '%Y-%m-%d %H:%M:%S')
+    delta = int((dt_a - dt_b).total_seconds())
+    return delta
 
 def is_alerted(packet_summary):
-    filepath = "/home/cowrie/modules/dos_detector/data/data_alerted"
-    is_exist = os.path.exists(filepath)
-    if is_exist:
+    filepath = "/home/cowrie/cowrie/var/log/cowrie/honeypot.log"
+    if os.path.exists(filepath):
         with open(filepath) as f:
             for line in f:
                 if packet_summary in line:
@@ -57,19 +62,16 @@ def is_alerted(packet_summary):
     else:
         return False
 
-def is_icmp_flood(packet_summary):
+def is_icmp_flood(packet_summary, datetime_now, host_ip):
     counter = 0
     filepath = "/home/cowrie/modules/dos_detector/data/data_ping_icmp"
     is_exist = os.path.exists(filepath)
     if is_exist:
         with open(filepath) as f:
-            for line_raw in f:
-                line_edited = remove_unsused(line_raw).strip().split(" ")
-                packet_edited = remove_unsused(packet_summary).split(" ")
-                if(packet_edited[0]==line_edited[0]):
-                    dt = line_edited[len(line_edited)-2] + " " + line_edited[len(line_edited)-1]
-                    date_time_obj = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-                    delta = int((datetime.today()-date_time_obj).total_seconds())
+            for line in f:
+                if get_src_ip(packet_summary) == get_src_ip(line) and get_src_ip(packet_summary) != host_ip:
+                    readed_datetime = get_timestamp(line)
+                    delta = count_delta(datetime_now, readed_datetime)
                     if delta == 0:
                         counter += 1
         if counter > 4:
@@ -87,22 +89,20 @@ def is_tcp_flood(packet_summary):
     if is_exist:
         with open(filepath) as f:
             for line_raw in f:
-                line_edited = remove_unsused(line_raw).strip().split(" ")
-                packet_edited = remove_unsused(packet_summary).split(" ")
-                if ":" in packet_edited:
-                    tmp = packet_edited[0].split(":")
+                pkt_src_ip = get_src_ip(packet_summary)
+                line_src_ip = get_src_ip(line_raw)
+                if ":" in pkt_src_ip:
+                    tmp = pkt_src_ip.split(':')
                     src_pkt = tmp[0]
                 else:
-                    src_pkt = packet_edited[0]
-                if ":" in line_edited:
-                    tmp = line_edited[0].split(":")
+                    src_pkt = pkt_src_ip
+                if ":" in line_src_ip:
+                    tmp = line_src_ip.split(':')
                     src_line = tmp[0]
                 else:
-                    src_line = packet_edited[0]
+                    src_line = line_src_ip
                 if(src_pkt == src_line):
-                    dt = line_edited[len(line_edited)-2] + " " + line_edited[len(line_edited)-1]
-                    date_time_obj = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-                    delta = int((datetime.today()-date_time_obj).total_seconds())
+                    delta = count_delta(get_timestamp(packet_summary), get_timestamp(line_raw))
                     if delta == 0:
                         counter += 1
         if counter > 0:
@@ -111,6 +111,23 @@ def is_tcp_flood(packet_summary):
             return False 
     else:
         return False
+
+def get_timestamp(packet_summary):
+    if '\n' in packet_summary:
+        packet_summary = packet_summary.strip()
+    packet = packet_summary.split(" ")
+    date = packet[len(packet)-2]+(' ')+packet[len(packet)-1]
+    return date
+    
+def get_src_ip(packet_summary):
+    packet = packet_summary.replace('Ether ', '')
+    packet = packet.replace(' IP ', '')
+    packet = packet.replace(' ICMP ', '')
+    packet = packet.replace(' TCP ', '')
+    packet = packet.replace('/ ', '')
+    packet = packet.replace('/', '')
+    splitted_packet = packet.split(' ')
+    return splitted_packet[0]
 
 def remove_unsused(line_raw):
     return remove_Ether(line_raw)
